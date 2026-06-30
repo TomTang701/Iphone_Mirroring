36bda8fd4055af2f22c094bbc8c5ef334123b7e9
Zixuan Tang <143047729+TomTang701@users.noreply.github.com>
2026-06-30 13:33:38 -0700

    Harden Windows launcher path

--- packaging/Start-iPhoneMirroring.cmd
+++ packaging/Start-iPhoneMirroring.cmd
@@ -1,3 +1,5 @@
 @echo off
 setlocal
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0Start-iPhoneMirroring.ps1" %*
+set "POWERSHELL=%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe"
+if not exist "%POWERSHELL%" set "POWERSHELL=powershell.exe"
+"%POWERSHELL%" -NoProfile -ExecutionPolicy Bypass -File "%~dp0Start-iPhoneMirroring.ps1" %*
